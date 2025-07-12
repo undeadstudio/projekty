ec3b91a8d40edd876de8787bd52058a3393ec7ce
undeadstudio <kontakt.3em@gmail.com>
2025-07-12 04:24:39 +0200

    Update rowery.js

--- rowery.js
+++ rowery.js
@@ -1,1 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function Marquee(selector, speed) {
+      const parent = document.querySelector(selector);
+      const content = parent.children[0];
 
+      const clone1 = content.cloneNode(true);
+      const clone2 = content.cloneNode(true);
+      parent.appendChild(clone1);
+      parent.appendChild(clone2);
+
+      let i = 0;
+      let fullWidth = content.offsetWidth * 3;
+
+      function animate() {
+        i += speed;
+        if (i >= fullWidth / 3) {
+          i = 0;
+        }
+        parent.scrollLeft = i;
+        requestAnimationFrame(animate);
+      }
+
+      animate();
+    }
+    Marquee('.marquee', 0.2);
+  });
+
+  $(document).ready(function(){
+    $(".custom-main-slider").owlCarousel({
+      items: 1,
+      loop: true,
+      dots: true,
+      nav: false,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      smartSpeed: 600
+    });
+  });
